8d0dc492257d26af2646dfcf223ce6dfc7ba778f
Zach Barbre <qfk6@cdc.gov>
2020-12-03 18:53:28 -0500

    day 2 part 2, clean up part 1

--- day2/day2.js
+++ day2/day2.js
@@ -9,35 +9,64 @@ try {
 }
 
 const passwordArray = data.split('\r\n');
-console.log(passwordArray.length)
-// const passwordArray = [
-//     '1-3 a: abcde',
-//     '1-3 b: cdefg',
-//     '2-9 c: ccccccccc',
-// ]
 
-const passwordObjectArray = passwordArray.map(passwordString => {
-    const passwordStrArr = passwordString.split(' ')
-    const passwordObj = {
-        min: parseInt(passwordStrArr[0].substring(0, passwordStrArr[0].indexOf('-'))),
-        max: parseInt(passwordStrArr[0].substring(passwordStrArr[0].indexOf('-') + 1)),
-        letter: passwordStrArr[1].substring(0, passwordStrArr[1].indexOf(':')),
-        password: passwordStrArr[2],
-    }
-    return passwordObj
-})
+const passwordNumberofValidOne = (passwordArray) => {
+    const passwordObjectArray = passwordArray.map(passwordString => {
+        const passwordStrArr = passwordString.split(' ')
+        const passwordObj = {
+            min: parseInt(passwordStrArr[0].substring(0, passwordStrArr[0].indexOf('-'))),
+            max: parseInt(passwordStrArr[0].substring(passwordStrArr[0].indexOf('-') + 1)),
+            letter: passwordStrArr[1].substring(0, passwordStrArr[1].indexOf(':')),
+            password: passwordStrArr[2],
+        }
+        return passwordObj
+    })
+    
+    const numberValid = passwordObjectArray.reduce((acc, passObj) => {
+        const regex = new RegExp(passObj.letter, 'g');
+        const arrOfLetterInPass = passObj.password.match(regex);
+    
+        if (!arrOfLetterInPass) return acc;
+    
+        if (passObj.min <= arrOfLetterInPass.length && arrOfLetterInPass.length <= passObj.max) {
+            return acc + 1;
+        }
+    
+        return acc
+    }, 0)
 
-const numberValid = passwordObjectArray.reduce((acc, passObj) => {
-    const regex = new RegExp(passObj.letter, 'g');
-    const arrOfLetterInPass = passObj.password.match(regex);
+    return numberValid;
+}
 
-    if (!arrOfLetterInPass) return acc;
+console.log(passwordNumberofValidOne(passwordArray));
 
-    if (passObj.min <= arrOfLetterInPass.length && arrOfLetterInPass.length <= passObj.max) {
-        return acc + 1;
-    }
+const passwordNumberValidTwo = (passwordArray) => {
+    const passwordObjectArray = passwordArray.map(passwordString => {
+        const passwordStrArr = passwordString.split(' ')
+        const passwordObj = {
+            index1: parseInt(passwordStrArr[0].substring(0, passwordStrArr[0].indexOf('-'))) - 1,
+            index2: parseInt(passwordStrArr[0].substring(passwordStrArr[0].indexOf('-') + 1)) - 1,
+            letter: passwordStrArr[1].substring(0, passwordStrArr[1].indexOf(':')),
+            password: passwordStrArr[2],
+        }
+        return passwordObj
+    })
 
-    return acc
-}, 0)
+    const numberValid = passwordObjectArray.reduce((acc, passObj) => {
+        if (passObj.password[passObj.index1] === passObj.letter && 
+            passObj.password[passObj.index2] === passObj.letter) {
+            return acc;
+        }
 
-console.log(numberValid)
+        if (passObj.password[passObj.index1] === passObj.letter || 
+            passObj.password[passObj.index2] === passObj.letter) {
+            return acc + 1;
+        }
+
+        return acc;
+    }, 0);
+
+    return numberValid;
+}
+
+console.log(passwordNumberValidTwo(passwordArray));
